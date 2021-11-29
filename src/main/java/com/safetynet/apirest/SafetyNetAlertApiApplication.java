6fef8dbefc612a04c9f6d33918be03a0299cb729
Pascal Chatard <pchatard@aol.com>
2021-11-29 15:16:28 +0100

    Creation projet, configuration pom.xml

---
+++ src/main/java/com/safetynet/apirest/SafetyNetAlertApiApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/apirest/SafetyNetAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
